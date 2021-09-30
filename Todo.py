5ae12da7cf4f7dd452b63f36c8eb4e67d054488b
whatsinmyopsec <kevin.power.97.kp@gmail.com>
2021-09-30 21:23:50 +0100

    Update Todo.py
    I changed your theme and updated your imports

--- Todo.py
+++ Todo.py
@@ -1,4 +1,6 @@
-from tkinter import *
+from tkinter import Button, Entry, Menu, Scrollbar, Tk, Frame, Listbox
+from tkinter.constants import ANCHOR, END, RIGHT, LEFT, BOTH
+
 from tkinter.font import Font
 from tkinter import filedialog
 import pickle
@@ -6,7 +8,6 @@ import pickle
 root = Tk()
 root.title('Nitesh-TODO List!')
 root.geometry("500x500")
-
 
 ############### Fonts ################
 
@@ -22,15 +23,17 @@ my_frame.pack(pady=10)
 
 ################# List Box #############
 my_list = Listbox(my_frame,
-           font=my_font,
-           width=25,
-           height=5,
-           bg="SystemButtonFace",
-           bd=0,
-           fg="#464646",
-           highlightthickness=0,
-           selectbackground="grey",
-           activestyle="none")
+                  font=my_font,
+                  width=25,
+                  height=5,
+                  bg="SystemButtonFace",
+                  background="#4a4a4a",
+                  bd=0,
+                  fg="#4fe3a3",
+                  highlightthickness=0,
+                  selectbackground="grey",
+                  activestyle="none"
+                  )
 
 my_list.pack(side=LEFT, fill=BOTH)
 
@@ -39,11 +42,11 @@ my_list.pack(side=LEFT, fill=BOTH)
 #stuff = ["Do daily Checkin","Do Event checkin","Complete Daily Task","Complete Weekly Task","Take a break"]
 
 ############# Add dummmy list to list box ##############
-#for item in stuff:
- #   my_list.insert(END, item)
-    
+# for item in stuff:
+#   my_list.insert(END, item)
+
 ################# Ceate Scrollbar ###########################
-my_scrollbar= Scrollbar(my_frame)
+my_scrollbar = Scrollbar(my_frame)
 my_scrollbar.pack(side=RIGHT, fill=BOTH)
 
 #################### Add Scrollbar ######################
@@ -51,104 +54,109 @@ my_list.config(yscrollcommand=my_scrollbar.set)
 my_scrollbar.config(command=my_list.yview)
 
 ################### ADD item entry box#################
-my_entry = Entry(root, font=("Helvetica", 24),width=24)
+my_entry = Entry(root, font=("Helvetica", 24), width=24, background="#9e9e9e")
 my_entry.pack(pady=20)
 
 ######################## Crete button frame ##########
-button_frame=Frame(root)
+button_frame = Frame(root)
 button_frame.pack(pady=20)
 
 ##################### Funnctions ###################
+
+
 def delete_item():
     my_list.delete(ANCHOR)
+
 
 def add_item():
     my_list.insert(END, my_entry.get())
     my_entry.delete(0, END)
 
+
 def cross_off_item():
     # Cross Off Item
     my_list.itemconfig(
         my_list.curselection(),
-        fg="#dedede")
+        fg="#d4483d")
     # Get rid of Selection bar
-    my_list.selection_clear(0,END)
-    
+    my_list.selection_clear(0, END)
+
+
 def uncross_item():
     # Cross Off Item
     my_list.itemconfig(
         my_list.curselection(),
-        fg="#464646")
+        fg="#4fe3a3")
     # Get rid of Selection bar
-    my_list.selection_clear(0,END)
-    
+    my_list.selection_clear(0, END)
+
+
 def delete_crossed():
     count = 0
     while count < my_list.size():
-        if my_list.itemcget(count, "fg")=="#dedede":
-           my_list.delete(my_list.index(count))
+        if my_list.itemcget(count, "fg") == "#dedede":
+            my_list.delete(my_list.index(count))
         else:
-            count+=1
-            
+            count += 1
+
+
 def save_list():
     file_name = filedialog.asksaveasfilename(
         initialdir="C:/gui/data",
         title="Save File",
-        filetypes=(("Text Files","*.txt")
-                   ,("All Files","*.*"))
-        ) 
+        filetypes=(("Text Files", "*.txt"), ("All Files", "*.*"))
+    )
     if file_name:
         if file_name.endswith(".txt"):
             pass
         else:
-            file_name=f'{file_name}.txt'
-            
-        #Delete Cross off items before saving 
+            file_name = f'{file_name}.txt'
+
+        # Delete Cross off items before saving
         count = 0
         while count < my_list.size():
-            if my_list.itemcget(count, "fg")=="#dedede":
+            if my_list.itemcget(count, "fg") == "#dedede":
                 my_list.delete(my_list.index(count))
             else:
-                count+=1
-                
+                count += 1
+
         # grab everything from the list
         stuff = my_list.get(0, END)
-        
+
         # open the selected file
         output_file = open(file_name, 'wb')
-        
-        #add data to the file
-        pickle.dump(stuff,output_file)
-            
-        
+
+        # add data to the file
+        pickle.dump(stuff, output_file)
+
 
 def open_list():
     file_name = filedialog.askopenfilename(
-           initialdir="C:/gui/data",
-            title="Open File",
-            filetypes=(("Text Files","*.txt"),
-                       ("All Files","*.*"))
+        initialdir="C:/gui/data",
+        title="Open File",
+        filetypes=(("Text Files", "*.txt"),
+                   ("All Files", "*.*"))
     )
     if file_name:
-        #DElete current open list
-        my_list.delete(0,END)
-        
-        #Open file name
-        input_file=open(file_name, 'rb')
-        
-        #Load the data from the  file
+        # DElete current open list
+        my_list.delete(0, END)
+
+        # Open file name
+        input_file = open(file_name, 'rb')
+
+        # Load the data from the  file
         stuff = pickle.load(input_file)
-        
-        #output data to the screen
+
+        # output data to the screen
         for item in stuff:
-            my_list.insert(END, item) 
-    
-    
+            my_list.insert(END, item)
+
+
 def delete_list():
-    my_list.delete(0,END)
-    
-    
-####################### Create Menu ##############    
+    my_list.delete(0, END)
+
+
+####################### Create Menu ##############
 
 my_menu = Menu(root)
 root.config(menu=my_menu)
@@ -167,20 +175,22 @@ file_menu.add_command(label="Clear List", command=delete_list)
 
 ################# Add Buttons ################
 
-delete_button = Button(button_frame, text="Delete Item",command=delete_item)
-add_button = Button(button_frame, text="Add Item",command=add_item)
-cross_off_button = Button(button_frame, text="Cross Item",command=cross_off_item)
-uncross_button = Button(button_frame, text="Uncross Item",command=uncross_item)
-delete_crossed_button = Button(button_frame, text="Delete Cross Item",command=delete_crossed)
+delete_button = Button(button_frame, text="Delete Item", command=delete_item)
+add_button = Button(button_frame, text="Add Item", command=add_item)
+cross_off_button = Button(
+    button_frame, text="Cross Item", command=cross_off_item)
+uncross_button = Button(
+    button_frame, text="Uncross Item", command=uncross_item)
+delete_crossed_button = Button(
+    button_frame, text="Delete Cross Item", command=delete_crossed)
 
 file_menu.add_separator()
 
-delete_button.grid(row=0,column=0)
-add_button.grid(row=0,column=1, padx=20)  
-cross_off_button.grid(row=0,column=2)  
-uncross_button.grid(row=0,column=3, padx=20) 
-delete_crossed_button.grid(row=0,column=4) 
+delete_button.grid(row=0, column=0)
+add_button.grid(row=0, column=1, padx=20)
+cross_off_button.grid(row=0, column=2)
+uncross_button.grid(row=0, column=3, padx=20)
+delete_crossed_button.grid(row=0, column=4)
 
 
-     
-root.mainloop()   
+root.mainloop()
